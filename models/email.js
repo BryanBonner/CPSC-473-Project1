56d20404b8acd61ef5bc7cd771e1256f0dbfe814
Jimmy Vo <Jimmyvo866@gmail.com>
2016-10-22 20:24:24 -0700

    fixed homepage to have register/post/login on same page

--- models/email.js
+++ models/email.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
-
+var http = require('http');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+app.set('view engine', 'ejs');
 
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport({
@@ -12,18 +20,20 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-app.get('/email', function(req, res) {
+app.get('/email', function(req,res){
+  res.render('submitPost');
+});
 
+app.post('/email', function(req, res) {
     var teacher = req.body.teacher;
-    var subject = req.body.subject;
+    var comment = req.body.comment;
     var email = req.body.email;
-
     // setup e-mail data with unicode symbols
     var mailOptions = {
-        from: '"Unlamented Epicurean" <atlas_cruz009@csu.fullerton.edu>', // sender address
-        to: 'atlascruz009@gmail.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world', // plaintext body
+        from: '"Unlamented Epicurean" <unlamented.epicurean@gmail.com>',
+        to: email,
+        subject: 'New Excuse Posted',
+        text: comment
     };
 
     // send mail with defined transport object
@@ -33,4 +43,5 @@ app.get('/email', function(req, res) {
         }
         console.log('Message sent: ', info.response);
     });
+
 });
